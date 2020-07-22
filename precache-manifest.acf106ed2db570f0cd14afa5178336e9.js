27d9b723cf8764966290a81bd0f05555f56e9e09
Lilit <lilit@inone.am>
2020-07-22 23:24:55 +0400

    Updates

---
+++ precache-manifest.acf106ed2db570f0cd14afa5178336e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8862564838367464e999789d287c601c",
+    "url": "/team_builder/index.html"
+  },
+  {
+    "revision": "d4d38b5b27675887d5d9",
+    "url": "/team_builder/static/css/main.056177a1.chunk.css"
+  },
+  {
+    "revision": "07af39d251021608ddb1",
+    "url": "/team_builder/static/js/2.f334afb4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/team_builder/static/js/2.f334afb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4d38b5b27675887d5d9",
+    "url": "/team_builder/static/js/main.b79b5d95.chunk.js"
+  },
+  {
+    "revision": "79955c59177ea51ff600",
+    "url": "/team_builder/static/js/runtime-main.f73fa546.js"
+  },
+  {
+    "revision": "12333ffe8a17ede83c2d63ec75cc1a45",
+    "url": "/team_builder/static/media/delete.12333ffe.svg"
+  },
+  {
+    "revision": "70ebbcd163470cdcdd46617ad9e91610",
+    "url": "/team_builder/static/media/heart.70ebbcd1.svg"
+  },
+  {
+    "revision": "a8cd76c1934dc020e510f93a33774305",
+    "url": "/team_builder/static/media/heart_full.a8cd76c1.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/topics/precache-manifest.8e8a07206db9a51a44933419d64a3f2c.js"
+  "/team_builder/precache-manifest.acf106ed2db570f0cd14afa5178336e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/topics/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/team_builder/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
