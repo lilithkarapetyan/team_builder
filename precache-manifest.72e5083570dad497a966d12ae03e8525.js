621554f9bba7122924516d822961e241c57ad4e5
Lilit <lilit@inone.am>
2020-07-22 18:29:57 +0400

    Updates

---
+++ precache-manifest.72e5083570dad497a966d12ae03e8525.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e32808afcb1bb0572c8ccbe2850f18",
+    "url": "/topics/index.html"
+  },
+  {
+    "revision": "e0998c7749402a8d4be4",
+    "url": "/topics/static/css/main.056177a1.chunk.css"
+  },
+  {
+    "revision": "5965c97dee65ac70a16b",
+    "url": "/topics/static/js/2.3305aa95.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/topics/static/js/2.3305aa95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0998c7749402a8d4be4",
+    "url": "/topics/static/js/main.6156fc66.chunk.js"
+  },
+  {
+    "revision": "6bb3fa346af1c5a0ef24",
+    "url": "/topics/static/js/runtime-main.518e20a8.js"
+  },
+  {
+    "revision": "12333ffe8a17ede83c2d63ec75cc1a45",
+    "url": "/topics/static/media/delete.12333ffe.svg"
+  },
+  {
+    "revision": "70ebbcd163470cdcdd46617ad9e91610",
+    "url": "/topics/static/media/heart.70ebbcd1.svg"
+  },
+  {
+    "revision": "a8cd76c1934dc020e510f93a33774305",
+    "url": "/topics/static/media/heart_full.a8cd76c1.svg"
+  }
+]);
